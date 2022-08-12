678355fda093d1b1deafd12513bf7bf5a2651db3
Matteo <110922392+f0r7y@users.noreply.github.com>
2022-08-12 10:27:23 +0200

    Create  Bulk update Azure AD with user attributes from CSV.ps1

---
+++ Bulk update Azure AD with user attributes from CSV.ps1
@@ -0,0 +1,33 @@
+# Connect to AzureAD
+Connect-AzureAD
+
+# Get CSV content
+$CSVrecords = Import-Csv C:\Temp\Test.csv -Delimiter ";"
+
+# Create arrays for skipped and failed users
+$SkippedUsers = @()
+$FailedUsers = @()
+
+# Loop trough CSV records
+foreach ($CSVrecord in $CSVrecords) {
+    $upn = $CSVrecord.UserPrincipalName
+    $user = Get-AzureADUser -Filter "userPrincipalName eq '$upn'"  
+    if ($user) {
+        try{
+        $user | Set-AzureADUser -Department $CSVrecord.Department -TelephoneNumber $CSVrecord.TelephoneNumber
+        } catch {
+        $FailedUsers += $upn
+        Write-Warning "$upn user found, but FAILED to update."
+        }
+    }
+    else {
+        Write-Warning "$upn not found, skipped"
+        $SkippedUsers += $upn
+    }
+}
+
+# Array skipped users
+# $SkippedUsers
+
+# Array failed users
+# $FailedUsers
